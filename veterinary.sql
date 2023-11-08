4f31cafb460b5a61e5bceb2219d5d09a0d0a78c5
Ken Angelo Velasquez <22-30918@g.batstate-u.edu.ph>
2023-11-08 12:28:17 +0800

    feat/rename-column-paymenttime

--- veterinary.sql
+++ veterinary.sql
@@ -147,3 +147,7 @@ VALUES
 	-- feat/add-column-registereddate
 ALTER TABLE owners
 ADD COLUMN registereddate DATE;
+
+	-- feat/rename-column-paymenttime
+ALTER TABLE invoices
+RENAME COLUMN paymentdate TO paymenttime;
